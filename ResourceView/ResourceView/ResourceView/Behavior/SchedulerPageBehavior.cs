eb91566fe2e12b8b7732ea11176de911068da84f
Nijamudeenm M <nijamudeensulaiman@syncfusion.com>
2019-06-28 16:56:44 +0530

    Basic resource view and temaplated resource view samples added

---
+++ ResourceView/ResourceView/ResourceView/Behavior/SchedulerPageBehavior.cs
@@ -0,0 +1,66 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ResourceView
+{
+    public class SchedulerPageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.Content.FindByName<SfSchedule>("schedule");
+            this.WireEvents();
+
+
+            var col = schedule.ViewHeaderStyle.DateTextColor;
+        }
+
+        private void WireEvents()
+        {
+            this.schedule.VisibleDatesChangedEvent += OnVisibleDatesChangedEvent;
+        }
+
+        private void OnVisibleDatesChangedEvent(object sender, VisibleDatesChangedEventArgs e)
+        {
+            var currentMonth = e.visibleDates[e.visibleDates.Count / 2].Month;
+            var intern = schedule.ScheduleResources.FirstOrDefault(emp => (emp as Employee).Id.ToString() == "8600");
+            if (currentMonth == 2 || currentMonth == 7)
+            {
+                if (intern != null)
+                    return;
+
+                Employee employee = new Employee();
+                employee.Name = "Sophiya";
+                employee.Id = "8600";
+                employee.Color = Color.FromHex("#FFE671B8");
+                employee.DisplayPicture = "schedule-resource-4.png";
+
+                schedule.ScheduleResources.Add(employee);
+            }
+            else
+            {
+                if (intern == null)
+                    return;
+
+                schedule.ScheduleResources.Remove(intern);
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.UnWireEvents();
+        }
+
+        private void UnWireEvents()
+        {
+            this.schedule.VisibleDatesChangedEvent -= OnVisibleDatesChangedEvent;
+        }
+    }
+}
---
+++ ResourceView/ResourceView/ResourceView/Model/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ResourceView
+{
+    /// <summary>   
+    /// Represents custom data properties.   
+    /// </summary> 
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets id
+        /// </summary>
+        public object Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets Color
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets display picture
+        /// </summary>
+        public string DisplayPicture { get; set; }
+    }
+}
